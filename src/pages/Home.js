10f4d2bd74f836460677f761a7761520ff3d4e97
abutler6588 <abutler6588@gmail.com>
2021-03-02 14:07:26 -0600

    completed sinlge room page and configured routing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,6 @@ import Banner from '../components/Banner';
 import {Link} from 'react-router-dom';
 import Services from '../components/Services';
 import FeaturedRooms from '../components/FeaturedRooms';
-
 export default function Home () {
     return (
         <>
@@ -20,6 +19,7 @@ export default function Home () {
     </Hero>
     <Services />
         <FeaturedRooms />
+        
     </>
     );
 }
